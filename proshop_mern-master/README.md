bffe60396300dafd8ca8796c1323103ea1ba0c38
Sarabjot1112 <singhpahwa1112@gmail.com>
2024-06-15 00:41:44 +0530

    first commit

---
+++ proshop_mern-master/README.md
@@ -0,0 +1,18 @@
+# Installation Command
+
+npm install && npm run data:import && cd frontend && npm install && cd ..
+
+# Run
+
+npm run dev
+
+# Sample User Logins
+
+admin@example.com (Admin)
+123456
+
+john@example.com (Customer)
+123456
+
+jane@example.com (Customer)
+123456
---
+++ proshop_mern-master/backend/data/products.js
@@ -0,0 +1,149 @@
+const products = [
+  {
+    name: 'Airpods Airpods',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 22990,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'iPhone 11 Pro 256GB',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 55990,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cannon EOS 80D',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 84999,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 34999,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 1999,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 4999,
+    countInStock: 20,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'JBL Flip 5',
+    image: '/images/JBL.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'JBL',
+    category: 'Electronics',
+    price: 9999,
+    countInStock: 40,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Home Theatre 4.1 ch',
+    image: '/images/sony.jpg',
+    description:
+      'Enjoy Movies & TV programs with family and let your home thunder with Powerful Bass and wider sound effect of Multimedia Speaker SA-D40. This stylish 80W 4.1ch multimedia speaker feature Bluetooth® connectivity along with USB and audio inputs, So you can enjoy your favourite mobile songs with enhanced sound. Just one cable required to connect and is easy to control by the simple and user friendly remote controller.',
+    brand: 'SONY',
+    category: 'Electronics',
+    price: 13990,
+    countInStock: 25,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'ASUS TUF Gaming F17',
+    image: '/images/asus.jpg',
+    description:
+      'Geared for serious gaming and real-world durability, the TUF Gaming F17 is a fully-loaded Windows 10 Home gaming laptop that can carry you to victory. Powered by the latest 10th Gen Intel® Core™ i7 CPU and GeForce® GTX 1650 Ti GPU, action-packed gameplay is fast, fluid, and fully saturates speedy IPS-level displays up to 120Hz to 144Hz. Even while boasting a smaller and more portable chassis than its predecessors, this gaming laptop also features a larger 48Wh battery for superior battery life. Efficient self-cleaning cooling combines with TUFs signature military-grade durability to make this battle-tested road warrior a powerful ally for any gamer.',
+    brand: 'ASUS',
+    category: 'Electronics',
+    price: 71999,
+    countInStock: 5,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'MSI GF63 i5',
+    image: '/images/MSI.jpg',
+    description:
+      'Clear and detailed visuals. Thanks to the Hi-resolution Audio feature, you can experience the power of music, movies, and more without any loss. This laptop features a keyboard that has a SteelSeries silver-lining printing keyboard that ensures aesthetic appeal and a lasting build quality.',
+    brand: 'MSI',
+    category: 'Electronics',
+    price: 54999,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'SAMSUNG Galaxy S24 Ultra',
+    image: '/images/Samsung.jpg',
+    description:
+      'Welcome to the era of mobile AI. With Galaxy S24 Ultra in your hands, you can unleash whole new levels of creativity, productivity and possibility — starting with the most important device in your life. Your smartphone,Epic, just like that.',
+    brand: 'SAMSUNG',
+    category: 'Electronics',
+    price: 129999,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'SONY Bravia X75L',
+    image: '/images/SonyLed.jpg',
+    description:
+      'Our smart TV delivers exciting content in 4K HDR. Even 2K pictures are upscaled to near 4K quality with sharp details and billions of real-world colours, reproduced by our 4K Processor X1™. Lifelike audio is fine-tuned for a thrilling experience.',
+    brand: 'SONY',
+    category: 'Electronics',
+    price: 99900,
+    countInStock: 70,
+    rating: 0,
+    numReviews: 0,
+  },
+
+]
+
+export default products
